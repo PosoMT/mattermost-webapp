a55d60b9f3fe2781858253f4ec2cda31ebb13e5b
geideko <geydeko@yandex.ru>
2020-10-07 14:47:42 +0300

    Fixed UI

--- components/user_settings/modal/user_settings_modal.tsx
+++ components/user_settings/modal/user_settings_modal.tsx
@@ -107,7 +107,7 @@ class UserSettingsModal extends React.PureComponent<Props, State> {
         super(props);
 
         this.state = {
-            active_tab: 'general',
+            active_tab: 'notifications',
             active_section: '',
             showConfirmModal: false,
             enforceFocus: true,
@@ -278,12 +278,14 @@ class UserSettingsModal extends React.PureComponent<Props, State> {
         }
         const tabs = [];
 
-        tabs.push({name: 'general', uiName: formatMessage(holders.general), icon: 'icon fa fa-gear', iconTitle: Utils.localizeMessage('user.settings.general.icon', 'General Settings Icon')});
-        tabs.push({name: 'security', uiName: formatMessage(holders.security), icon: 'icon fa fa-lock', iconTitle: Utils.localizeMessage('user.settings.security.icon', 'Security Settings Icon')});
+        
         tabs.push({name: 'notifications', uiName: formatMessage(holders.notifications), icon: 'icon fa fa-exclamation-circle', iconTitle: Utils.localizeMessage('user.settings.notifications.icon', 'Notification Settings Icon')});
         tabs.push({name: 'display', uiName: formatMessage(holders.display), icon: 'icon fa fa-eye', iconTitle: Utils.localizeMessage('user.settings.display.icon', 'Display Settings Icon')});
-        tabs.push({name: 'sidebar', uiName: formatMessage(holders.sidebar), icon: 'icon fa fa-columns', iconTitle: Utils.localizeMessage('user.settings.sidebar.icon', 'Sidebar Settings Icon')});
         tabs.push({name: 'advanced', uiName: formatMessage(holders.advanced), icon: 'icon fa fa-list-alt', iconTitle: Utils.localizeMessage('user.settings.advance.icon', 'Advanced Settings Icon')});
+
+        if(this.props.currentUser.roles && this.props.currentUser.roles.split(" ").includes("service_user")) {
+            tabs.push({name: 'security', uiName: formatMessage(holders.security), icon: 'icon fa fa-lock', iconTitle: Utils.localizeMessage('user.settings.security.icon', 'Security Settings Icon')});
+        }
 
         return (
             <Modal
